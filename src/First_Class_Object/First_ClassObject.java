63549d33c2db1a956ac90bc371238b65a47eb8e4
rock0 <rock0@KALPESH-MEVADA>
2025-12-06 22:31:10 +0530

    commit

--- src/First_Class_Object/First_ClassObject.java
+++ src/First_Class_Object/First_ClassObject.java
@@ -6,11 +6,15 @@ public class First_ClassObject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		First_ClassObject a1 = new First_ClassObject(); // how memory address looks like and how represented
+		First_ClassObject a1 = new First_ClassObject(); // how memory address looks like and how represented (It show hexadecimal value and everytime different)
 	    System.out.println(" Non Static Member access by Object created with new keyword:" +a1.i); // To access non-static member 
 	    System.err.println(" Static Member access by Class:" +First_ClassObject.j);
 	    System.out.println("Address of Object in Hexadecimal and everytime object created memory address will be differetn"+" : "+a1); // how memory address looks like and how represented
-
+	    /*Output
+	     *  Non Static Member access by Object created with new keyword:10
+ 			Static Member access by Class:20
+			Address of Object in Hexadecimal and everytime object created memory address will be differetn : First_Class_Object.First_ClassObject@8efb846
+	     */
 	}
 
 }
